5a545da54a1ff1bed15bf0e4921c6634f2810443
Karuna3laka-W <weblookboomkavi@gmail.com>
2026-02-23 10:06:34 +0530

    fix: force https for mixed conte123nt error

--- hotel-system/app/Providers/AppServiceProvider.php
+++ hotel-system/app/Providers/AppServiceProvider.php
@@ -21,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Force HTTPS in production environments
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
     }
 }
